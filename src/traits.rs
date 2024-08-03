4ee7506ecd7537a6ba2eff517f48c4a952a60c57
Cheng <61931277+ChengCY-2254@users.noreply.github.com>
2024-08-03 13:30:07 +0800

    upd

--- src/traits.rs
+++ src/traits.rs
@@ -1,25 +1,10 @@
-use async_trait::async_trait;
+use crate::models::Chapters;
 
-///```rust
-/// struct Demo;
-///
-/// #[async_trait]
-/// impl ParseDocument for Demo {
-///     type OutPut = ();
-///
-///     async fn parse(_driver: &'_ WebDriver) -> Result<Self::OutPut, WebDriverError> {
-///         todo!()
-///     }
-/// }
-///
-/// ```
-/// 用于解析网页文档并转换为自己的数据模型
-/// 
-///
-#[async_trait]
-pub trait ParseDocument {
+pub type Driver = fantoccini::Client;
+pub type By<'a> = fantoccini::Locator<'a>;
+
+pub trait ParseWith<T = Driver> {
     type OutPut;
-    async fn parse(
-        driver: &'_ thirtyfour::WebDriver,
-    ) -> Result<Self::OutPut, thirtyfour::error::WebDriverError>;
+    type Error;
+    async fn parse_with(_driver: &'_ T) -> Result<Self::Output, Self::Error>;
 }
